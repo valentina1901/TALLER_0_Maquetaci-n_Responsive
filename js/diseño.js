b1f573ee54452cd86387a18452f9a6a2b0884b54
valentina1901 <vgaviria415@misena.edu.co>
2022-10-05 18:43:09 -0500

    arreglos

--- js/diseño.js
+++ js/diseño.js
@@ -5,3 +5,22 @@ function alert(){
    } );
 
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+setInterval(()=>{
+    
+  plusSlides(1)
+},5000)
